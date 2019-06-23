888a884f846b163083bddff9a8d16853ca9b4caa
syiming <syiming@umich.edu>
2019-06-23 12:34:35 -0400

    Update main to run loaddata

--- Model/main.py
+++ Model/main.py
@@ -9,14 +9,20 @@ Main function that combine different section.
 
 """
 
+from Data.data import *
+
+class config():
+    batch_size = 128
+    embedding_size = 256
+    skip_window = 1  # How many words to consider left and right.
+    num_skips = 2  # How many times to reuse an input to generate a context.
 
 
 def main():
     # Load data
     print("Start loading data...")
-
+    user, business = load_grouped_data()
     print("Data loading finished...")
-    print("Total data loaded: ")
 
     # Embedding Data
 
